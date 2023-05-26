72a549fb9053d354d4f618199a8e0fab3e1be516
Alex <fctcalderon@gmail.com>
2023-05-26 19:04:09 +0200

    done

--- complementosPHP/create.tables.php
+++ complementosPHP/create.tables.php
@@ -7,7 +7,6 @@ if (!isset($_SESSION['SESSION_EMAIL'])) {
 }
 
 if (isset($_POST['guardar'])) {
-
     $query = mysqli_query($conn, "SELECT * FROM usuarios WHERE email = '{$_SESSION['SESSION_EMAIL']}' OR username = '{$_SESSION['SESSION_EMAIL']}'");
 
     if (mysqli_num_rows($query) > 0) {
@@ -17,9 +16,10 @@ if (isset($_POST['guardar'])) {
 
     $tablas = $_POST['tablas'];
     $horario = $_POST['horario'];
-    $tblasAgregadas = [];
-    foreach ($tablas as $index => $tabla){
+    $tablasAgregadas = [];
+    $tareasAgregadas = []; // Array para almacenar las tareas agregadas
 
+    foreach ($tablas as $index => $tabla) {
         $nombreTabla = $tabla;
 
         $insert_tablas = mysqli_query($conn, "INSERT INTO tablas (nombre, id_usuario, horario) VALUES ('$nombreTabla', '$id', '$horario')") or die($conn->error);
@@ -27,39 +27,38 @@ if (isset($_POST['guardar'])) {
         if ($insert_tablas) {
             $tablaId = mysqli_insert_id($conn);
             $tablasAgregadas[] = $nombreTabla;
-        }else{
+
+            $tareas = $_POST['tareas'][$index];
+            $importancias = $_POST['importancias'][$index];
+            $fechas = $_POST['fechas'][$index];
+
+            // Insertar las tareas en la base de datos
+            foreach ($tareas as $taskIndex => $tarea) {
+                $nombreTarea = $tarea;
+                $importancia = $importancias[$taskIndex];
+                $fechaTarea = $fechas[$taskIndex];
+
+                // Insertar la tarea en la base de datos con el id_tabla obtenido
+                $query = "INSERT INTO tareas (tarea, importancia, fecha_tarea, id_tabla) VALUES ('$nombreTarea', $importancia, '$fechaTarea', $tablaId)";
+                $insert_tareas = mysqli_query($conn, $query);
+
+                if ($insert_tareas) {
+                    $tareasAgregadas[] = $nombreTarea; // Agregar la tarea al array de tareas agregadas
+                } else {
+                    echo "Error al agregar la tarea $nombreTarea: " . mysqli_error($conn);
+                }
+            }
+        } else {
             echo "Error al agregar la tabla $nombreTabla: " . mysqli_error($conn);
         }
-
-        $tareas = $_POST['tareas'];
-        $importancias = $_POST['importancias'];
-        $fechas = $_POST['fechas'];
-    
-        $tareasAgregadas = []; // Array para almacenar las tareas agregadas
-    
-        // Insertar las tareas en la base de datos
-        foreach ($tareas as $index => $tarea) {
-            $nombreTarea = $tarea;
-            $importancia = $importancias[$index];
-            $fechaTarea = $fechas[$index];
-    
-            // Insertar la tarea en la base de datos con el id_tabla obtenido
-            $query = "INSERT INTO tareas (tarea, importancia, fecha_tarea, id_tabla) VALUES ('$nombreTarea', $importancia, '$fechaTarea', $tablaId)";
-            $insert_tareas = mysqli_query($conn, $query);
-    
-            if ($insert_tareas) {
-                $tareasAgregadas[] = $nombreTarea; // Agregar la tarea al array de tareas agregadas
-            } else {
-                echo "Error al agregar la tarea $nombreTarea: " . mysqli_error($conn);
-            }
-        }
     }
+
     if (!empty($tablasAgregadas)) {
-        echo "Las tablas: " . implode(", ", $tablasAgregadas) . " se han agregado correctamente <br>";
+        echo "Las tablas: " . implode(", ", $tablasAgregadas) . " se han agregado correctamente<br>";
     }
 
     if (!empty($tareasAgregadas)) {
-        echo "Las tareas:" . implode(", ", $tareasAgregadas) . " se han agregado correctamente";
+        echo "Las tareas: " . implode(", ", $tareasAgregadas) . " se han agregado correctamente";
     }
 }
 ?>
